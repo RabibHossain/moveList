c8381f6aa62ac08de16a7926d506d4747aa1142a
RabibHossain <galib.rabib@gmail.com>
2021-08-20 02:27:55 +0600

    status code

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -5,6 +5,7 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from watchlist_app.models import Movie
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
 # Function Based Views 
@@ -21,31 +22,40 @@ def movie_lists(request):
         serializer = MovieSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
 
     if request.method == 'GET':
 
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
 
         return Response(serializer.data)
 
     if request.method == 'PUT':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie, data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.error)
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
         movie.delete()
 
-        return Response({"message": "Data deleted successfully"})
+        return Response(status=status.HTTP_204_NO_CONTENT)
